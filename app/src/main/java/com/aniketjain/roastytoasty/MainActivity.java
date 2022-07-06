ec08e071f39d4e72bb7feb9ebdbe95089e710d72
aniketjain8441 <aniketjain8441@gmail.com>
2022-07-06 19:18:34 +0530

    master

--- app/src/main/java/com/aniketjain/roastytoasty/MainActivity.java
+++ app/src/main/java/com/aniketjain/roastytoasty/MainActivity.java
@@ -11,7 +11,6 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-//        Toasty.normal(this, "Hello World, My name is aniket.");
         Toasty.custom(this, "Hello World", R.drawable.ic_launcher_background, "#ff9933", 300, 300);
     }
 }
